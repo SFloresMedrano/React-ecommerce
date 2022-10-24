32d1c7dc62e29e27c0253966f9c479b3171812e0
Simón Flores <asfloresmedrano@gmail.com>
2022-10-23 22:46:04 -0300

    First Task React Course CoderHouse

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,10 @@
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+export default function CartWidget(){
+    return(
+        <div className="cartWidget">
+        <ShoppingCartIcon size="2em"/>
+    </div>
+    );  
+
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer (){
+    return(
+        <ul>
+            <li>Inicio</li>
+            <li>Productos</li>
+            <li>Contacto</li>
+        </ul>
+    )
+}
