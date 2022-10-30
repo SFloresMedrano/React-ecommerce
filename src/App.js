4695edc685aaaa4f4f8d267eae5b915507ed93d4
Simón Flores <asfloresmedrano@gmail.com>
2022-10-29 21:55:17 -0300

    Añadido boton

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import './App.css';
-import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import ItemListContainer from './components/ItemListContainer';
-
-
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <>
       <Navbar/>
-      <ItemListContainer greeting={'Simon'} style={{ 
-        background: '#2E3B55',
-        fontSize: '2 rem' }}/>
+      <ItemListContainer greeting={'Simon'}/>
       <Footer/>
     </>
   );
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,13 +1,18 @@
 import  React from "react";
+import ItemCount from "./ItemCount";
 
 export default function ItemListContainer({greeting}){
-    
+    const onAdd =(quantity)=>{
+        console.log(`Compraste ${quantity} unidades`)
+    }
+
     return(
      <div style={{ 
         background: '#2E3B55',
         height:50,
         color: 'red' }}>
         <p style={{ fontSize: 25,textAlign: 'center' }}>Bienvenido a la pagina, {greeting}</p>
+        <ItemCount initial={1} stock={5} onAdd={onAdd}/>
      </div>
     )
 }
