720ef4f4455de9738e1aa10eadee48d7620e7238
Ramiro <ramifazio@gmail.com>
2022-11-02 19:53:56 -0300

    Sugerencias!

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import ItemListContainer from './components/ItemListContainer';
-
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import ItemListContainer from "./components/ItemListContainer";
 
 function App() {
-
   return (
     <>
-      <Navbar/>
-      <ItemListContainer greeting={'Simon'} style={{ 
-        background: '#2E3B55',
-        fontSize: '2 rem' }}/>
-      <Footer/>
+      <Navbar />
+      <ItemListContainer
+        greeting={"Simon"}
+        style={{
+          background: "#2E3B55",
+          fontSize: "2 rem",
+        }}
+      />
+      <Footer />
     </>
   );
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,13 +1,17 @@
-import  React from "react";
+import React from "react";
 
-export default function ItemListContainer({greeting}){
-    
-    return(
-     <div style={{ 
-        background: '#2E3B55',
-        height:50,
-        color: 'red' }}>
-        <p style={{ fontSize: 25,textAlign: 'center' }}>Bienvenido a la pagina, {greeting}</p>
-     </div>
-    )
+export default function ItemListContainer({ greeting }) {
+  return (
+    <div
+      style={{
+        background: "#2E3B55",
+        height: 50,
+        color: "red",
+      }}
+    >
+      <p style={{ fontSize: 25, textAlign: "center" }}>
+        Bienvenido a la pagina, {greeting}
+      </p>
+    </div>
+  );
 }
